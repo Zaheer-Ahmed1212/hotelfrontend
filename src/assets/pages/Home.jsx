fe34f720904bfca27f585510abd0e6cd13e9dacf
Zaheer-Ahmed1212 <zahirjokhio105@gmail.com>
2026-09-21 13:55:55 +0500

    change

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
 import Intro from '../Components/Intro'
 import SignatureStays from '../Components/SignatureStays'
@@ -7,7 +6,7 @@ import ArdeaWay from '../Components/ArdeaWay'
 import BeyondTheRoom from '../Components/BeyondTheRoom'
 import Testomonials from '../Components/Testomonials'
 import Statistics from '../Components/Statistics'
-import FinalCTA from '../Components/FinalCta'
+import FinalCTA from '../Components/FinalCTA'
 import Footer from '../Components/Footer'
 
 function Home() {
